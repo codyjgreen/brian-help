28482796ee9ac9c2456ca0a34836da388640ae68
bbpak <brianbpak@gmail.com>
2019-04-13 16:00:41 -0700

    heroku init

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  Rails.application.routes.draw do
+    namespace :api do
+      namespace :v1 do
+        resources :games, only: [:index, :create, :update]
+        resources :players, only: [:index, :create, :update]
+      end
+    end
+  end
+end
